23fbcacd9f704b36d46251d561e9ea4a4467e6ea
Eduard <eduard@unicorn-stc.de>
2024-12-14 01:51:40 +0200

    Change url to real devices localhost

--- lib/utils/url.dart
+++ lib/utils/url.dart
@@ -1,12 +1,12 @@
 abstract class Url {
   /// Base URL for the web socket server
-  static const String baseUrl = 'http://10.0.2.2:3001';
+  static const String baseUrl = 'http://localhost:3001';
 
   /// Login URL for the web socket server
   static const String loginUrl = '$baseUrl/login';
 
   /// Web socket URL for the web socket server
-  static const String wsUrl = 'ws://10.0.2.2:3001';
+  static const String wsUrl = 'ws://localhost:3001';
 
   /// Chat URL for the web socket server
   static const String chatUrl = '$wsUrl/chat';
